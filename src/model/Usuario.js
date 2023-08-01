3eacc1f4af175ffa6ad097802fa047ad967f4a3a
murilotrigo_ <murilo.trigo@auditornfe.com.br>
2023-08-01 16:38:55 -0300

    feat: verificando se o email já existe

--- src/model/Usuario.js
+++ src/model/Usuario.js
@@ -3,10 +3,11 @@ const db = new DataBase;
 
 class Usuario {
 
-    async insereUsuarioDB(nome, email, senha, incdate) {
+    async insereUsuarioDB(nome, email, senha) {
+        const user_incate = new Date();
         try {
             const sql = `INSERT INTO usuarios (user_nome, user_email, user_senha, user_incdate) VALUES (?, ?, ?, ?);`;
-            const values = [nome, email, senha, incdate];
+            const values = [nome, email, senha, user_incate];
             const data = await db.pool.query(sql, values);
             console.log('Usuário inserido com sucesso!');
             return data; // Se necessário, você pode retornar os dados inseridos ou outra informação relevante
@@ -57,6 +58,18 @@ class Usuario {
             const sql = `SELECT * FROM usuarios WHERE user_nome = ?;`;
             const data = await db.pool.query(sql, nome);
             console.log(`O usuário ${nome} está cadastrado`, data);
+            return data[0]; // Se necessário, você pode retornar os dados encontrados ou outra informação relevante
+        } catch (error) {
+            console.error('Erro ao buscar usuários no banco:', error);
+            return { erro: error, msg: `Erro ao buscar usuários no banco.` };
+        }
+    }
+
+    async buscarUsuariosByEmailDB(email) {
+        try {
+            const sql = `SELECT user_nome,user_email FROM usuarios WHERE user_email = ?;`;
+            const data = await db.pool.query(sql, email);
+            console.log(`O e-mail ${email} foi encontrado`, data);
             return data[0]; // Se necessário, você pode retornar os dados encontrados ou outra informação relevante
         } catch (error) {
             console.error('Erro ao buscar usuários no banco:', error);
